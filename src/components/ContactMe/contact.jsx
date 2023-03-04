b5c008ecdc6a32f5b7bfe939b2b0be36a64fd21e
lavanmetta <lavanmetta111@gmail.com>
2023-03-04 16:25:49 +0530

    firebase

--- src/components/ContactMe/contact.jsx
+++ src/components/ContactMe/contact.jsx
@@ -1,15 +1,81 @@
 import React from 'react'
 import './contact.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { getAboutMe } from '../Data/data';
 import SocialMedia from '../SocialMedia/socialmedia';
+import { ThreeDots } from  'react-loader-spinner'
 
 export default function Contact() {
     const [about, setAbout] = useState('')
+    const [msg, setMsg] = useState('')
+    const [loader, setLoader] = useState(false)
+    const [error, setError] = useState('')
+    const [data, setData] = useState({
+      name: '',
+      email: '',
+      number: '',
+      message: '',
+    })
+    const formRef = useRef(null);
 
     useEffect(() => {
-        setAbout(getAboutMe());
-    }, [])
+      setAbout(getAboutMe());
+  }, [])
+
+
+    const postData = async e => {
+      e.preventDefault()
+      setLoader(true)
+      const {name, email, number, message} = data
+  
+      // eslint-disable-next-line no-unused-vars
+
+    if (name.length === 0 || email.length === 0 || number.length === 0 || message.length === 0) {
+      setError('Required all inputs must')
+      setMsg('')
+      setLoader(false)
+    }  else {
+      setLoader(true)
+      // eslint-disable-next-line no-unused-vars
+      const res = await fetch(
+         'https://personal-cb3c9-default-rtdb.firebaseio.com/portfolio.json',
+         {
+           method: 'POST',
+           headers: {
+             'Content-Type': 'application/json',
+           },
+           body: JSON.stringify({
+             name,
+             email,
+             number,
+             message,
+           }),
+         },
+         // eslint-disable-next-line
+       ).then((response) => {
+          if (response.status === 200) {
+              setMsg('Form Successfully Submitted')
+              formRef.current.reset(); 
+              setLoader(false)
+              setError('')
+          }
+          else {
+            setMsg('')
+            setLoader(false)
+             setError('Form not Submitted / Internal Server Error. please send an Email')
+          }
+       })
+     }
+    }
+      
+
+
+
+
+    
+
+
+
 
   return (
     <div className='contact-container' id="contact">
@@ -48,15 +114,27 @@ export default function Contact() {
 
            <div className='contact-me-container'>
            
-           <form>
+           <form ref={formRef}>
            
                 <div className='form-container'>
                 <h4><i className="ri-service-fill lg-h"></i> I welcome your messages and feedback</h4>
-                    <input type="text" placeholder='Full Name' />
-                    <input type="text" placeholder='Email Address' />
-                    <input type="text" placeholder='Phone Number' />
-                    <textarea placeholder='Message...'/>
-                    <button>Send Message <i className="ri-send-plane-fill"></i></button>
+                    <input  onChange={e => setData({...data, name: e.target.value})} type="text" placeholder='Full Name' />
+                    <input  onChange={e => setData({...data, email: e.target.value})} type="email" placeholder='Email Address' />
+                    <input  onChange={e => setData({...data, number: e.target.value})} type="tel" maxLength='10'  placeholder='Phone Number' />
+                    <textarea  onChange={e => setData({...data, message: e.target.value})} placeholder='Message...'/>
+                    <p className='msg'>{msg}</p>
+                    <p className='msg-e'>{error}</p>
+                    <ThreeDots 
+                           height="40" 
+                           width="40" 
+                           radius="9"
+                           color="#ffffff" 
+                           ariaLabel="three-dots-loading"
+                           wrapperStyle={{}}
+                           wrapperClassName=""
+                           visible={loader}
+                           />
+                    <button onClick={postData}>Send Message <i className="ri-send-plane-fill"></i></button>
                 </div>
             </form>
            </div>
